3799cdda4f271bec0761abfc9552fcb563af6e9b
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-15 18:37:30 -0300

    in process

--- app/speechRecognition.js
+++ app/speechRecognition.js
@@ -1,12 +1,41 @@
 const guessElement = document.getElementById('guess')
+const muteBtn = document.querySelector('.nav-btn')
+
+const submitBtn = document.querySelector('#send')
+
+submitBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  const guessBox = document.querySelector('#writen-guess')
+  const guess = guessBox.value
+  showGuessOnScreen(guess)
+  checkIfGuessIsValid(guess)
+})
 
 window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-br'
-recognition.start()
-recognition.addEventListener('result', onSpeak)
+
+function isUnMuted(){
+  return muteBtn.classList[1] == 'active'
+}
+
+muteBtn.addEventListener('click', () => {
+  muteBtn.classList.toggle('active')
+  guessBox.classList.toggle('active')
+  if(isUnMuted()){
+      listen()
+      muteBtn.innerHTML = `<button class="nav-btn"><i class="fa-solid fa-microphone"></i></button>`
+  }else {
+      muteBtn.innerHTML = `<button class="nav-btn"><i class="fa-solid fa-microphone-slash"></i></button>`
+  }
+})
+
+function listen(){
+  recognition.start()
+  recognition.addEventListener('result', onSpeak)
+}
 
 function onSpeak(e){
   const guess = e.results[0][0].transcript 
@@ -22,5 +51,6 @@ function showGuessOnScreen(guess){
 }
 
 recognition.addEventListener('end', () => {
-  recognition.start()
+recognition.start()
 })
+
